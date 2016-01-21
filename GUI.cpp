f69c5f92d4bf22a00fa3f43a727ea7ef8e72f4ba
user.email <Igel101@web.de>
2016-01-21 18:06:32 +0100

    GUI more completed

--- GUI.cpp
+++ GUI.cpp
@@ -8,6 +8,9 @@
 #include <wx/sizer.h>
 #include <wx/button.h>
 #include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/gbsizer.h>
+#include <wx/wx.h>
 #include "include/Strings.h"
 
 #define ID_Hello 1
@@ -22,8 +25,8 @@ END_EVENT_TABLE()
 GUI::GUI()
 {
 	Create(nullptr, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("id"));
-	SetMinSize(wxSize(500, 500));
-	SetMaxSize(wxSize(500, 500));
+	SetMinSize(wxSize(500, 650));
+	SetMaxSize(wxSize(500, 650));
 	SetWindowStyleFlag(wxWINDOW_VARIANT_MAX);
 	SetTitle(std::string("Stegano+ V.").append(VERSION));
 	Center();
@@ -35,6 +38,54 @@ GUI::GUI()
     wxFont *headerFont = new wxFont(30, wxMODERN, wxNORMAL, wxNORMAL);
     headerLabel->SetFont(*headerFont);
     verticalSizer->Add(headerLabel, 0, wxCENTER, 0);
+
+    wxGridBagSizer *form = new wxGridBagSizer(20, 10);
+    wxBitmap *openDialogButtonIcon = new wxBitmap("./res/folder.png", wxBITMAP_TYPE_PNG);
+   // wxFont *descriptorFont = new wxFont(10, wxNORMAL, wxNORMAL, wxNORMAL);
+    wxStaticText *containerFileLabel = new wxStaticText(this, wxID_ANY, "Kontainer-Datei: ", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+ //   containerFileLabel->SetFont(*descriptorFont);
+    wxTextCtrl *containerFileInput = new wxTextCtrl(this, wxID_ANY, _T(""), wxDefaultPosition, wxSize(240, -1), wxALIGN_LEFT);
+    containerFileInput->SetEditable(false);
+    containerFileInput->Disable();
+
+    wxBitmapButton *openContainerDialogButton = new wxBitmapButton(this, wxID_ANY, *openDialogButtonIcon, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    wxRadioButton *hideFileRadio = new wxRadioButton(this, wxID_ANY, "Datei verstecken");
+
+    wxStaticText *hideFileLabel = new wxStaticText(this, wxID_ANY, "zu versteckende Datei: ", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+ //   containerFileLabel->SetFont(*descriptorFont);
+    wxTextCtrl *hideFileInput = new wxTextCtrl(this, wxID_ANY, _T(""), wxDefaultPosition, wxSize(240, -1), wxALIGN_LEFT);
+    hideFileInput->SetEditable(false);
+    hideFileInput->Disable();
+    wxBitmapButton *openHideFileDialogButton = new wxBitmapButton(this, wxID_ANY, *openDialogButtonIcon, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    wxRadioButton *hideTextRadio = new wxRadioButton(this, wxID_ANY, "Text verstecken");
+
+    wxTextCtrl *textToHideCtrl = new wxTextCtrl(this, wxID_ANY, _T(""), wxDefaultPosition, wxSize(440, 200), wxTE_MULTILINE | wxNO_BORDER);
+
+    wxButton *startHidingButton = new wxButton(this, wxID_ANY, _T("Start!"), wxDefaultPosition, wxSize(200, 40));
+
+    form->Add(containerFileLabel, wxGBPosition(0, 0));
+    form->Add(containerFileInput, wxGBPosition(0, 1));
+    form->Add(openContainerDialogButton, wxGBPosition(0, 2));
+
+    form->Add(hideFileRadio, wxGBPosition(1, 0));
+    form->Add(new wxStaticText(this, wxID_ANY, _T("")), wxGBPosition(1, 1));
+    form->Add(new wxStaticText(this, wxID_ANY, _T("")), wxGBPosition(1, 2));
+
+    form->Add(hideFileLabel, wxGBPosition(2, 0));
+    form->Add(hideFileInput, wxGBPosition(2, 1));
+    form->Add(openHideFileDialogButton, wxGBPosition(2, 2));
+
+    form->Add(hideTextRadio, wxGBPosition(3, 0));
+    form->Add(new wxStaticText(this, wxID_ANY, _T("")), wxGBPosition(3, 1));
+    form->Add(new wxStaticText(this, wxID_ANY, _T("")), wxGBPosition(3, 2));
+
+
+    form->Add(textToHideCtrl, wxGBPosition(4, 0), wxGBSpan(1, 3));
+
+    form->Add(startHidingButton, wxGBPosition(5, 0), wxGBSpan(1, 3));
+    verticalSizer->Add(form, 0, wxEXPAND | wxLEFT | wxRIGHT | wxTOP | wxBOTTOM, 30);
+
+
     SetSizer(verticalSizer);
 
 	Connect(wxID_ANY,wxEVT_CLOSE_WINDOW,(wxObjectEventFunction)&GUI::OnClose);
